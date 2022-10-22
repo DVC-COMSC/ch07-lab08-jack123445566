3994c829a74a64db13a8536e7b0a68b142b81061
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-10-22 00:21:29 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# ******************************
-# Make your Code
-# ******************************
+numbers = [10, 15, 25, 30, 35]
+
+insval = int(input())
+
+for i in range(len(numbers)):
+    if numbers[i] > insval:
+        numbers.insert(i, insval)
+        break
+else:  
+    numbers.append(insval)
+
+print(numbers)
